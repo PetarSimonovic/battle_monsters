44de8746f787c47749653df03587a9098ffa5aed
Louis Wickremeratne <louiswickremeratne@yahoo.co.uk>
2021-01-21 15:50:15 +0000

    POST/ redirect /GET pattern

--- app.rb
+++ app.rb
@@ -2,15 +2,23 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     'Testing infrastructure working!'
     erb(:index)
   end
 
-  post '/play' do
-    @player_one = params[:player_one]
-    @player_two = params[:player_two]
-    erb(:play)
+  get '/play' do
+    @player_one = session[:player_one]
+    @player_two = session[:player_two]
+    erb :play
+  end
+
+  post '/names' do
+    session[:player_one] = params[:player_one]
+    session[:player_two] = params[:player_two]
+    redirect '/play'
   end
 
   run! if app_file == $0
