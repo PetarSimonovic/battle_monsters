4686d7940b0543ca6fa3ed94ba9347495ae195aa
Hugh Cavanagh <hacavanagh@gmail.com>
2021-01-22 16:18:41 +0000

    Adding tests for player one/two methods

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,8 +3,31 @@ require 'game.rb'
 
 describe "Game" do
 
-  # subject(:game) {Game.new("claude", "pete")}
 
+  context 'when first created' do
+    let(:player_one_name) { "Pete" }
+    let(:player_two_name) { "Hugh" }
+    let(:game) { Game.new(player_one_name, player_two_name) }
+
+    describe '#player_one' do
+      it 'returns player object' do
+        expect(game.player_one).to be_an_instance_of(Player)
+      end
+      it 'returns a player object with the right name' do
+        expect(game.player_one.name).to eq player_one_name
+      end
+    end
+
+    describe '#player_two' do
+      it 'returns player object' do
+        expect(game.player_two).to be_an_instance_of(Player)
+      end
+      it 'returns a player object with the right name' do
+        expect(game.player_two.name).to eq player_two_name
+      end
+    end
+
+  end
 
   it "reduces a player's hit_points after attack" do
     game = Game.new("claude", "pete")
