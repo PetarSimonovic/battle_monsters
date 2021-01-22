77ec68d2dc4d111371f60e94b9ee2a492c753cc3
PetarSimonovic <petar@simonovic.com>
2021-01-22 11:57:27 +0000

    Refactored

--- lib/player.rb
+++ lib/player.rb
@@ -4,15 +4,19 @@ class Player
   attr_reader :name
   attr_reader :hit_points
 
-  HIT_POINTS = 60
+  HIT_POINTS_START = 60
   HIT = 10
 
   def initialize(name)
     @name = name
-    @hit_points = HIT_POINTS
+    @hit_points = HIT_POINTS_START
   end
 
-  def hit
+  def attack(player)
+    player.receive_damage
+  end
+
+  def receive_damage
     @hit_points -= HIT
   end
 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,9 +2,24 @@ require 'player.rb'
 
 describe "Player" do
 
+  subject(:claude) { Player.new('Claude') }
+  subject(:pete) { Player.new('Pete') }
+
+
   it "stores the player name" do
-    player = Player.new("Claude")
-    expect(player.name).to eq("Claude")
+    expect(claude.name).to eq("Claude")
+  end
+
+  it "has hit points" do
+    expect(claude.hit_points).to eq(Player::HIT_POINTS_START)
+  end
+
+  it "receives damage" do
+    expect { claude.receive_damage }.to change{ claude.hit_points }.by(-Player::HIT)
+  end
+
+  it "reduces hit_points after attack" do
+    expect { pete.attack(claude) }.to change{ claude.hit_points }.by(-Player::HIT)
   end
 
 end
