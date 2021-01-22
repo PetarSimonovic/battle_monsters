c34216832b19aec7e73f2a0b0d8e43d29a47ca5f
PetarSimonovic <petar@simonovic.com>
2021-01-22 10:20:23 +0000

    Spelling errors corrected

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,5 +1,5 @@
 feature "Viewing hit points" do
-  scenario "Player one can view Plater two's hit points" do
+  scenario "Player One can view Player Two's hit points" do
     sign_in_and_play
     expect(page).to have_content "Claude: 60"
   end
